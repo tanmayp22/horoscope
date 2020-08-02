17b69673be018e4a93e4a71ecc8fa2da8a2ee88c
Hitesh <joshihitesh090@gmail.com>
2020-08-02 20:58:40 +0530

    capitalize strings
    decoration purpose only

--- utils/constants.dart
+++ utils/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 /// This file contains all the routing constants used within the app
 const String homeRoute = '/';
 const String ariesRoute = '/aries';
@@ -12,3 +14,29 @@ const String sagittariusRoute = '/sagittarius';
 const String capricornRoute = '/capricorn';
 const String aquariusRoute = '/aquarius';
 const String piscesRoute = '/pisces';
+
+// Colors
+const kBackgroundColor = Color(0xFFFEFEFE);
+const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLightColor = Color(0xFF959595);
+const kInfectedColor = Color(0xFFFF8748);
+const kDeathColor = Color(0xFFFF4848);
+const kRecovercolor = Color(0xFF36C12C);
+const kPrimaryColor = Color(0xFF3382CC);
+final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
+
+// Text Style
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kSubTextStyle = TextStyle(fontSize: 16, color: kTextLightColor);
+
+const kTitleTextstyle = TextStyle(
+  fontSize: 15,
+  color: kTitleTextColor,
+  fontWeight: FontWeight.bold,
+);
--- utils/router.dart
+++ utils/router.dart
@@ -11,51 +11,99 @@ class Router {
 
       case ariesRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen("aries", color: 0xFF298000));
+            builder: (_) => InfoScreen(
+                  "Aries",
+                  color: 0xFF298000,
+                  date: "Mar 21 -Apr 19",
+                ));
 
       case taurusRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('taurus', color: 0xFF8C0000));
+            builder: (_) => InfoScreen(
+                  'Taurus',
+                  color: 0xFF8C0000,
+                  date: "Apr 20 - May 20",
+                ));
 
       case geminiRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('gemini', color: 0xFFFEE605));
+            builder: (_) => InfoScreen(
+                  'Gemini',
+                  color: 0xFFFEE605,
+                  date: "May 21 - Jun 20",
+                ));
 
       case cancerRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('cancer', color: 0xFF6E6E6E));
+            builder: (_) => InfoScreen(
+                  'Cancer',
+                  color: 0xFF6E6E6E,
+                  date: "Jun 21 - Jul 22",
+                ));
 
       case leoRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('leo', color: 0xFFFF7E06));
+            builder: (_) => InfoScreen(
+                  'Leo',
+                  color: 0xFFFF7E06,
+                  date: "Jul 23 - Aug 22",
+                ));
 
       case virgoRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('virgo', color: 0xFFDA20B1));
+            builder: (_) => InfoScreen(
+                  'Virgo',
+                  color: 0xFFDA20B1,
+                  date: "Aug 23 - Sept 22",
+                ));
 
       case libraRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('libra', color: 0xFF8A06DB));
+            builder: (_) => InfoScreen(
+                  'Libra',
+                  color: 0xFF8A06DB,
+                  date: "Sept 23 - Oct 22",
+                ));
 
       case scorpioRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('scorpio', color: 0xFF59FF0B));
+            builder: (_) => InfoScreen(
+                  'Scorpio',
+                  color: 0xFF59FF0B,
+                  date: "Oct 23 - Nov 21",
+                ));
 
       case sagittariusRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('sagittarius', color: 0xFF0FE2FF));
+            builder: (_) => InfoScreen(
+                  'Sagittarius',
+                  color: 0xFF0FE2FF,
+                  date: "Nov 22 - Dec 21",
+                ));
 
       case capricornRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('capricorn', color: 0xFF14008E));
+            builder: (_) => InfoScreen(
+                  'Capricorn',
+                  color: 0xFF14008E,
+                  date: "Dec 22 - Jan 19",
+                ));
 
       case aquariusRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('aquarius', color: 0xFF8B5C30));
+            builder: (_) => InfoScreen(
+                  'Aquarius',
+                  color: 0xFF8B5C30,
+                  date: "Jan 20 - Feb 18",
+                ));
 
       case piscesRoute:
         return MaterialPageRoute(
-            builder: (_) => InfoScreen('pisces', color: 0xFF6D28FF));
+            builder: (_) => InfoScreen(
+                  'Pisces',
+                  color: 0xFF6D28FF,
+                  date: "Feb 19 - Mar 20",
+                ));
 
       default:
         return MaterialPageRoute(
